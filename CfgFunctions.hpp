ea103fa87a455e4df7776900e9e3324a0482ccee
AqChrome <coniferous.developers@gmail.com>
2025-08-17 18:12:31 +0300

    close project tablet

--- CfgFunctions.hpp
+++ CfgFunctions.hpp
@@ -1,29 +1,15 @@
+#define HALs_DEF_FUNCTIONS
+#include "core\economy\HALs\Addons\main\config.hpp"
+#undef HALs_DEF_FUNCTIONS
+
 class AQ {
 	tag = "AQ";
-		class Needs {
-			file = "core\survival\needs\inventory";
-			class onInventoryDblClick {};
-			class tryEat {};
-			class tryDrink {};
-		};
-};
-	
-class AQTB {
-	tag = "AQTB";
-	class Tablet {
-		file = "aq_tablet\functions";
-		class init {};
-		class registerAction {};
-		class open {};
-		class close {};
-		class buildHome {};
-		class onHomeClick {};
-		class openTasks {};
-		class openMapEmbedded {};
-		class setTasks {};
+	class Needs {
+		file = "core\survival\needs\inventory";
+		class onInventoryDblClick {};
+		class tryEat {};
+		class tryDrink {};
 	};
 };
 
-    #define HALs_DEF_FUNCTIONS
-    #include "core\economy\HALs\Addons\main\config.hpp"
-    #undef HALs_DEF_FUNCTIONS
+    
--- aq_tablet/functions/fn_onHomeClick.sqf
+++
@@ -1,6 +0,0 @@
-params ["_app"];
-switch (_app) do {
-  case "tasks": { [] call AQTB_fnc_openTasks; };
-  case "map":   { [] call AQTB_fnc_openMapEmbedded; };
-  default {};
-};
